1f7459c8ec641224815a41723f8b8c7c6401c987
kissrose-w <ai20210430@163.com>
2025-12-02 22:27:52 +0800

    Add num variable and log its value

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 console.log(222);
 
+const num = 110;
 const addZero = n => n < 10 ? '0' + n : n;
 
 console.log('hfbksdv');
 
 const randomNum = (max, min) => Math.floor(Math.random() * (max - min + 1) - min);
+
+console.log(num + 100);
